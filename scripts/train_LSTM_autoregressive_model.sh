66247e3532711a9608e96e5241991c3ecbd01976
lg519 <lg519@ic.ac.uk>
2023-04-11 21:22:25 +0100

    added set -e flag

--- scripts/train_LSTM_autoregressive_model.sh
+++ scripts/train_LSTM_autoregressive_model.sh
@@ -2,6 +2,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=<lg519> # required to send email notifcations - please replace <your_username> with your college login name or email address
+
+set -e
+
 export PATH=/vol/bitbucket/${USER}/HeartRateForecasting/HeartRateForecasting_env/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
