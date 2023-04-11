170bb346e5507c39b66a3d66430b7a994e226110
lg519 <lg519@ic.ac.uk>
2023-04-11 22:14:12 +0100

    more debug

--- scripts/train_LSTM_autoregressive_model.sh
+++ scripts/train_LSTM_autoregressive_model.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=<lg519> # required to send email notifcations - please replace <your_username> with your college login name or email address
-#SBATCH –output=LSTM_training.out
+#SBATCH –output=<LSTM_training>%j.out
 
 
 export PATH=/vol/bitbucket/${USER}/HeartRateForecasting/HeartRateForecasting_env/bin/:$PATH
@@ -14,12 +14,12 @@ uptime
 echo "STEP 1"
 
 # use cuda 11.2
-if [ -f /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh ]
-    then
-        . /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh
-    else
-        echo "CUDA 11.2.1 not found"
-fi
+# if [ -f /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh ]
+#     then
+#         . /vol/cuda/11.2.1-cudnn8.1.0.77/setup.sh
+#     else
+#         echo "CUDA 11.2.1 not found"
+# fi
 
 # run the python scripts
 cd /vol/bitbucket/${USER}/HeartRateForecasting
