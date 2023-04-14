fa40c281bf39dc0999252da192928e00bb767883
lg519 <lg519@ic.ac.uk>
2023-04-14 09:58:28 +0100

    fixed data preprocessing

--- models/helpers/preprocess_data.py
+++ models/helpers/preprocess_data.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 
 
 # Function to check if there's any nested array with a 0
-def has_zero(arr):
+def filter_data(arr):
     for nested_arr in arr:
-        if nested_arr[0] == 0:
+        if nested_arr[0] == 0 or nested_arr[0] > 1000:
             return True
+
     return False
 
 
@@ -18,24 +19,39 @@ def preprocess_data(df):
     # Extract breathing rate data
     breathing_rate_data = df["BR"]
 
-    print(len(heart_rate_data))
+    # print(len(heart_rate_data))
 
-    # Apply the has_zero function to each heart rate row
-    zero_check_hr = heart_rate_data.apply(has_zero)
-    zero_check_br = breathing_rate_data.apply(has_zero)
-
-    # Filter the rows based on the check results
+    # Filter the rows
+    zero_check_hr = heart_rate_data.apply(filter_data)
     heart_rate_data = heart_rate_data[~zero_check_hr].reset_index(drop=True)
     breathing_rate_data = breathing_rate_data[~zero_check_hr].reset_index(drop=True)
-    print(len(heart_rate_data))
+    # print(len(heart_rate_data))
 
+    zero_check_br = breathing_rate_data.apply(filter_data)
     heart_rate_data = heart_rate_data[~zero_check_br].reset_index(drop=True)
     breathing_rate_data = breathing_rate_data[~zero_check_br].reset_index(drop=True)
-    print(len(heart_rate_data))
-    # Scale the data between -1 and 1
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    heart_rate_data = [scaler.fit_transform(hr) for hr in heart_rate_data]
-    breathing_rate_data = [scaler.fit_transform(br) for br in breathing_rate_data]
+    # print(len(heart_rate_data))
+
+    # Concatenate each row of HR and BR data
+    concatenated_hr = np.concatenate(heart_rate_data)
+    concatenated_br = np.concatenate(breathing_rate_data)
+    # print(concatenated_br)
+
+    # Plot the concatenated data
+    # plt.plot(concatenated_br)
+    # plt.show()
+
+    # Scale the data
+    hr_scaler = MinMaxScaler()
+    br_scaler = MinMaxScaler()
+
+    # Fit the scaler to the concatenated data
+    hr_scaler.fit(concatenated_hr)
+    br_scaler.fit(concatenated_br)
+
+    # Transform the data
+    heart_rate_data = [hr_scaler.transform(hr) for hr in heart_rate_data]
+    breathing_rate_data = [br_scaler.transform(br) for br in breathing_rate_data]
 
     # Combine HR and BR data
     combined_data = [
@@ -49,7 +65,7 @@ def preprocess_data(df):
     val_df = combined_data[int(n * 0.7) : int(n * 0.9)]
     test_df = combined_data[int(n * 0.9) :]
 
-    return train_df, val_df, test_df
+    return train_df, val_df, test_df, hr_scaler, br_scaler
 
 
 def plot_samples(train_df, num_samples=3):
@@ -71,5 +87,5 @@ def plot_samples(train_df, num_samples=3):
 if __name__ == "__main__":
     # Example usage
     df = pd.read_pickle("SportDB.pkl")
-    train_df, val_df, test_df = preprocess_data(df)
+    train_df, val_df, test_df, _, _ = preprocess_data(df)
     plot_samples(train_df)
